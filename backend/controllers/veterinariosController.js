8ab8a114bf9d9df0781b097126c884fa3301e445
soteloagustinn <soteloagustinn@gmail.com>
2025-08-24 02:57:53 -0300

    identificar si el usuario existe, revisar si un usuario ha confirmado su cuenta para autenticarlo y comprobar el password del usuario

--- backend/controllers/veterinariosController.js
+++ backend/controllers/veterinariosController.js
@@ -48,8 +48,35 @@ const confirmar = async (req, res) => {
     
     } catch (error) {
         console.log(error)
-    }
-    
-}
+    };
+};
 
-export{registrar, perfil, confirmar}
+const autenticar = async (req, res) => {
+    const {email, password} = req.body
+
+
+    //Comprobar si el usuario existe
+    const usuario = await Veterinario.findOne({email})
+
+    if(!usuario) {
+        const error = new Error ('El Usuario no existe');
+        return res.status(404).json({msg: error.message});
+    };
+
+    //comprobar si el usuario esta confirmado
+    if(!usuario.confirmado){
+        const error = new Error('Tu cuenta no ha sido confirmada');
+        return res.status(403).json({ msg: error.message})
+    }
+
+    //Revisar el password
+    if(await usuario.comprobarPassword(password )){
+        //Autentica
+    }else{
+        const error = new Error("El password es incorrecto");
+        return res.status(403).json({msg: error.message});
+    }
+
+};
+
+export{registrar, perfil, confirmar, autenticar};
--- backend/routes/veterinarioRoutes.js
+++ backend/routes/veterinarioRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { registrar, perfil, confirmar} from '../controllers/veterinariosController.js';
+import { registrar, perfil, confirmar, autenticar} from '../controllers/veterinariosController.js';
 
 router.post("/", registrar )
 router.get("/perfil", perfil );
 router.get("/confirmar/:token", confirmar);
+router.post('/login', autenticar)
 
 export default router;
