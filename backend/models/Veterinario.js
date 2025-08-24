59a684ae357261af72a585ac38d014cbcf6b5c96
soteloagustinn <soteloagustinn@gmail.com>
2025-08-24 01:55:04 -0300

    hasheo de password

--- backend/models/Veterinario.js
+++ backend/models/Veterinario.js
@@ -1,4 +1,5 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+import bcrypt from "bcrypt";
 import generarId from '../helpers/generarId.js'
 
 const veterinarioSchema = mongoose.Schema({
@@ -35,6 +36,17 @@ const veterinarioSchema = mongoose.Schema({
         default: false
     }
 });
+
+veterinarioSchema.pre('save', async function(next){
+    if(!this.isModified("password")){
+        next();
+    }
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+});
+
+
+
 
 const Veterinario = mongoose.model("Veterinarios", veterinarioSchema);
 export default Veterinario;
